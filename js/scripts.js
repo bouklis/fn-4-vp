6924f2c281f24786bfd45a2f6e84821224e6a037
Bouklis K <booklees@gmail.com>
2017-04-07 18:38:59 +0300

    Hero for deskop

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 jQuery(function( $ ) {
 	'use strict';
 
+
+	// Calculate viewport height
+	// and set hero height equal to inner viewport
+	// =================================================
 	var innerHeight =  window.innerHeight;
 
 	$('.hero-upper').css("height", innerHeight * 0.65 );
 	$('.hero-lower').css("height", innerHeight * 0.35 );
+
+
 
 	//  Language Chooser Pop up
 	// ===============================
@@ -23,25 +29,27 @@ jQuery(function( $ ) {
 	$('.service-item-button').click(function() {
 		// Close all tabs
 
-		var $boxHeight1 = $(this).siblings('.accordion-item-content').height();
-
 		if ($(this).parent().hasClass('open') ) {
-			$(this).parent().removeClass('open');
+			// Close accordion item
+			$('.accordion-item').removeClass('open');
+			console.log('ffr');
+
+			// Remove position fixed
+			$(this).removeClass('fixed');
 		} else {
 			$('.accordion-item').removeClass('open');
 			$(this).parent().addClass('open');
 		}
 
-		var $boxHeight = $(this).siblings('.accordion-item-content').height();
-
-		// console.log($(this).parent().scrollTop());
-		// console.log($boxHeight1, $boxHeight);
-		// console.log($(this).parent().hasClass('open'));
 	});
 
 	// On click accordion-item
 	// stick it smoothly on top of the window
 	$('.accordion-item').click(function() {
+
+		// $('.accordion-item').removeClass('open');
+		// $(this).addClass('open');
+
 		var x = $(this).offset().top;
 		var y = $(window).scrollTop();
 
@@ -49,7 +57,37 @@ jQuery(function( $ ) {
 	        'scrollTop' : $(this).position().top
 	    });
 
-		console.log(x, y);
+		// $(this).find('.service-item-button').addClass('fixed');
+		// $(this).find('.accordion-item-content').css('margin-top', '56px');
+
+		// console.log(x, y);
+	});
+
+	$(window).scroll(function() {
+		var x = $('.accordion-item.open').offset().top;
+		var y = $(window).scrollTop();
+		var itemHeight = $('.accordion-item.open').height();
+
+		console.log(x, y, itemHeight);
+
+		// if ( x < y - itemHeight + 56) {
+		// 	 $('.accordion-item.open .service-item-button').removeClass('fixed');
+		// 	 $('.accordion-item.open .service-item-button').addClass('absolute');
+		// }
+		// if ( x < y - itemHeight) {
+		// 	$('.accordion-item.open .service-item-button').removeClass('absolute');
+		// }
+		// if ( x < y - itemHeight) {
+		// 	 $('.accordion-item.open .service-item-button').removeClass('fixed');
+		// }
+		// if ( x > y ) {
+		// 	 $('.accordion-item.open .service-item-button').removeClass('fixed');
+		// }
+		//
+		// if ($('.accordion-item').hasClass('open')) {
+		// 	// $('.accordion-item.open .service-item-button').removeClass('fixed');
+		//
+		// }
 	});
 
 
@@ -67,8 +105,6 @@ jQuery(function( $ ) {
 		$('.after-hero .text-3').css("margin-right", st  / 10 );
 		$('.after-hero .line-1').css("right", 40 + st  / 10 );
 		$('.after-hero .line-2').css("right", 18 + st  / 7 );
-
-		console.log(innerHeight);
 
 	});
 
